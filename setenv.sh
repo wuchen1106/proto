711d1e51b91de9af87fe73904ddf325007d9f26d
wuchen <wuchen1106@gmail.com>
2015-07-01 19:24:57 +0900

    added new files

---
+++ setenv.sh
@@ -0,0 +1,23 @@
+export MYG4SIMWORKROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#for configuration cards
+export PHYSICSLIST=QGSP_BERT                 # QGSP_BERT;QGSP_BERT_HP;QGSP_BERT_noRadi;QGSP_BERT_HP_noRadi
+export MATERIALLISTROOT=material_list
+export MAGFIELDCARDROOT=mag/MagField_none
+export OUTCARDROOT=output/output_test
+export GEOCARDROOT=geometry_test
+export GENFILEROOT=gen/gen_cosmic
+
+#for directories
+export MACROSROOT=$MYG4SIMWORKROOT/macros
+export MACROSCOMMONROOT=$MACROSROOT/common
+export CONFIGUREROOT=$MYG4SIMWORKROOT/configure
+export MYG4SIMDATAROOT="$MYG4SIMWORKROOT/data/"
+export CONFIGUREDATAROOT=$MYG4SIMDATAROOT
+export MYG4SIMFIELDMAPSROOT="$MYG4SIMWORKROOT/fieldMaps/"
+export FIELDMAPSROOT=$MYG4SIMFIELDMAPSROOT
+export OFILENAMEROOT=$MYG4SIMWORKROOT/output/raw_g4sim.root
+export RUNNAMEROOT=TEST
+export LOGFILEROOT=$MYG4SIMWORKROOT/runlog/logfile
+
+source $MYG4SIMROOT/env.sh
